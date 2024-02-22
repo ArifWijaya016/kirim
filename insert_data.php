0bde3d325cf6ea432b2b6c8e0f7c90bc5c492fae
Arif Wijaya <101441049+ArifWijaya016@users.noreply.github.com>
2024-02-22 11:59:32 +0700

    Update insert_data.php

--- insert_data.php
+++ insert_data.php
@@ -14,18 +14,12 @@ if ($conn->connect_error) {
 }
 
 // Mengambil data dari ESP32
-$tds = isset($_POST['tds']) ? $_POST['tds'] : false;
-$temp_hidro = isset($_POST['temp_hidro']) ? $_POST['temp_hidro'] : false;
+$tds = $_POST['tds'];
+$temp_hidro = $_POST['temp_hidro'];
 
-// Cek apakah nilai tds dan temp_hidro ada
-if ($tds === false || $temp_hidro === false) {
-    echo "Error: Missing tds or temp_hidro values.";
-    $conn->close();
-    exit();
-}
-
-// Menyimpan data ke database
-$sql = "INSERT INTO $table (tds, temp_hidro, nama) VALUES ($tds, $temp_hidro, 'hidroponik')";
+// Menyimpan data ke database dengan kolom 'nama' yang bernilai 'hidroponik'
+$sql = "INSERT INTO $table (tds, temp_hidro, nama) 
+VALUES ($tds, $temp_hidro, 'hidroponik')";
 
 if ($conn->query($sql) === TRUE) {
     echo "Data inserted successfully";
